5f1c93543e9aed67e300c9476f1304e9422f76e4
Naoya Kawasaki <qqks23sd@gmail.com>
2020-12-10 00:37:39 +0900

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React, { ReactElement } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { createStyles, CssBaseline, makeStyles } from "@material-ui/core";
+import Header from "./layout/Header";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+  })
+);
 
 const App: React.FC = (): ReactElement => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+const Header: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <span>Header</span>
+    </div>
+  );
+};
+
+export default Header;
