dae343549a62098e078be98162d947508bfc7eb1
Naoya Kawasaki <qqks23sd@gmail.com>
2020-12-11 23:45:07 +0900

    create header component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,53 @@
 import React, { ReactElement } from "react";
+import {
+  AppBar,
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      // [theme.breakpoints.up("sm")]: {
+      //   width: `calc(100% - ${drawerWidth}px)`,
+      //   marginLeft: drawerWidth,
+      // },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+  })
+);
 
 const Header: React.FC = (): ReactElement => {
+  const classes = useStyles();
   return (
-    <div>
-      <span>Header</span>
-    </div>
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h5" noWrap>
+          Header
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
